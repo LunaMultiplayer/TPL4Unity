a868b65d35a4a847a15d39c8a2d422c5d697eb59
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-09-30 12:18:41 +0200

    Fix assembly info

--- System.Threading/Properties/AssemblyInfo.cs
+++ System.Threading/Properties/AssemblyInfo.cs
@@ -1,37 +1,17 @@
-﻿//
-//  AssemblyInfo.cs
-//
-//  Author:
-//  	Jim Borden  <jim.borden@couchbase.com>
-//
-//  Copyright (c) 2015 Couchbase, Inc All rights reserved.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+[assembly: AssemblyTitle("TPL4Unity")]
+[assembly: AssemblyDescription("Task Parallel Library for Unity (.NET 3.5)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gabriel Vazquez")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-[assembly: AssemblyTitle("System.Threading.Tasks.Net35")]
-[assembly: AssemblyDescription("The Task Parallel Library ported back to .NET 3.5")]
-[assembly: AssemblyCompany ("Couchbase")]
-[assembly: AssemblyProduct("TPL .NET 3.5")]
-[assembly: AssemblyCopyright("2015")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: ComVisible(false)]
+[assembly: Guid("e1fac97f-1306-4bc0-a4ac-0e5f0afbc473")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
